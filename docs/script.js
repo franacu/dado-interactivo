39bf5807fc3c791db951da7f74fe11df5fab2721
Francisco Acuña <127992216+franacu@users.noreply.github.com>
2025-08-08 12:19:48 -0300

    Implementación completa del cubo 3D interactivo

--- docs/script.js
+++ docs/script.js
@@ -1,28 +1,25 @@
-let currentRotation= { x:0 , y:0 };
+let currentRotation = { x: 0, y: 0 };
 
-function rotateCube(direction)
-{
-    switch(direction)
-    {
+function rotateCube(direction) {
+    switch(direction) {
         case 'left':
-            currentRotation.y -=90;
+            currentRotation.y -= 90;
             break;
         case 'right':
-            currentRotation.y +=90;
+            currentRotation.y += 90;
             break;    
         case 'up':
-            currentRotation.x +=90;
+            currentRotation.x += 90;
             break;
         case 'down':
-            currentRotation.x -=90;
+            currentRotation.x -= 90;
             break;    
     }
     document.getElementById('cube').style.transform = 
-    `rotateX(${currentRotation.x}deg) rotateY(${currentRotation.y}deg)`; 
+        `rotateX(${currentRotation.x}deg) rotateY(${currentRotation.y}deg)`;
 }
 
 function reset() {
     currentRotation = { x: 0, y: 0 };
     document.getElementById('cube').style.transform = 'rotateX(0deg) rotateY(0deg)';
 }
-
