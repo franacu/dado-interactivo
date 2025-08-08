7208e0a9e9340d4c2e3857eef30f762008996f89
Francisco Acuña <127992216+franacu@users.noreply.github.com>
2025-08-08 12:12:15 -0300

    Muevo JS y CSS a /docs para compatibilidad

---
+++ docs/script.js
@@ -0,0 +1,28 @@
+let currentRotation= { x:0 , y:0 };
+
+function rotateCube(direction)
+{
+    switch(direction)
+    {
+        case 'left':
+            currentRotation.y -=90;
+            break;
+        case 'right':
+            currentRotation.y +=90;
+            break;    
+        case 'up':
+            currentRotation.x +=90;
+            break;
+        case 'down':
+            currentRotation.x -=90;
+            break;    
+    }
+    document.getElementById('cube').style.transform = 
+    `rotateX(${currentRotation.x}deg) rotateY(${currentRotation.y}deg)`; 
+}
+
+function reset() {
+    currentRotation = { x: 0, y: 0 };
+    document.getElementById('cube').style.transform = 'rotateX(0deg) rotateY(0deg)';
+}
+
